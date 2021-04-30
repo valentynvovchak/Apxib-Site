e0932b222d27683b5db5b602fa803b55aeb9daf6
Cotyk <valentyn.vovchak@gmail.com>
2021-04-30 16:13:17 +0300

    Last 6

--- trynewblog/prod_settings.py
+++ trynewblog/prod_settings.py
@@ -11,15 +11,15 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['apxib.site', 'apxib', 'www.apxib.site', 'www.apxib', '127.0.0.1', 'apxib-site.herokuapp.com']
 
-# DATABASES = {
-#     'default': dj_database_url.config()
-# }
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
@@ -31,9 +31,6 @@ MANAGERS = (('Valiunya', 'valiunyavovchak@gmail.com'),)
 # S3 BUCKETS CONFIG
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-print()
-print(os.environ.get('AWS_ACCESS_KEY_ID'), '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-print()
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
