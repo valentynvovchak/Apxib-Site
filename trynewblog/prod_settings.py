c7f72ffd04407110a44fbf6107b6e08f31b1768e
Cotyk <valentyn.vovchak@gmail.com>
2021-04-30 16:04:52 +0300

    Last 3

--- trynewblog/prod_settings.py
+++ trynewblog/prod_settings.py
@@ -1,10 +1,10 @@
 import os
 import dj_database_url
-from boto.s3.connection import S3Connection
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = S3Connection(os.environ.get('SECRET_KEY'))
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 DEBUG = False
@@ -32,9 +32,9 @@ MANAGERS = (('Valiunya', 'valiunyavovchak@gmail.com'),)
 
 # S3 BUCKETS CONFIG
 
-AWS_ACCESS_KEY_ID = S3Connection(os.environ.get('AWS_ACCESS_KEY_ID'))
-AWS_SECRET_ACCESS_KEY = S3Connection(os.environ.get('AWS_SECRET_ACCESS_KEY'))
-AWS_STORAGE_BUCKET_NAME = S3Connection(os.environ.get('AWS_STORAGE_BUCKET_NAME'))
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = True
 AWS_DEFAULT_ACL = None
